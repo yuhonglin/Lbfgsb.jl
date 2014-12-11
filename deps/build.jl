8c027dcb062f4c7bb82f2a27c6697124e418f450
Honglin Yu <yuhonglin1986@gmail.com>
2014-12-12 04:34:44 +1100

    descard BinDeps in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,48 +1,47 @@
-# it seems that we need to write the "deps.jl" ourselves
+## it seems that we need to write the this file ourselves
 
-using BinDeps
+# get the current path
+currentFilePath = @__FILE__()
+currentDirPath = dirname(currentFilePath)
 
-@BinDeps.setup
-
-lbfgsbf = library_dependency("lbfgsbf", aliases=["liblbfgsbf"])
-
+# function to mkdir lib dir
 function mklibdir()
-    depdir = BinDeps.depsdir(lbfgsbf);
-    usrdir = joinpath(depdir, "usr");
+    usrdir = joinpath(currentDirPath, "usr");
     
     if isdir(usrdir) == false
         mkdir(usrdir);
     end
 
-    usrdir = joinpath(depdir, "lib");
-    if isdir(usrdir) == false
-        mkdir(usrdir);
+    libdir = joinpath(usrdir, "lib");
+    if isdir(libdir) == false
+        mkdir(libdir);
     end
 end
 
+# run make file
 function runmake()
-    usrdir = joinpath(BinDeps.depsdir(lbfgsbf), "usr", "lib");
+    usrdir = joinpath(currentDirPath, "usr", "lib");
     run(`make OUTPUTDIR=$(usrdir)`)
+    run(`rm -r $(currentDirPath)/Lbfgsb.3.0`)
 end
 
-
+# write the "deps.jl" file
 function writeDeps()
-    prefix = joinpath(BinDeps.depsdir(lbfgsbf))
-    libpath = joinpath(prefix, "usr", "lib", "liblbfgsbf.so")
-    outputfile = open(joinpath(prefix, "deps.jl"), "w");
+    libpath = joinpath(currentDirPath, "usr", "lib", "liblbfgsbf.so")
+    outputfile = open(joinpath(currentDirPath, "deps.jl"), "w");
     write( outputfile, "macro checked_lib(libname, path)\n    (dlopen_e(path) == C_NULL) && error(\"Unable to load \\n\\n\$libname (\$path)\n\nPlease re-run Pkg.build(package), and restart Julia.\")\n    quote const \$(esc(libname)) = \$path end\nend\n@checked_lib liblbfgsbf \"$(libpath)\"\n")
+    close(outputfile)
 end
 
-provides(Sources, { URI("http://users.iems.northwestern.edu/~nocedal/Software/Lbfgsb.3.0.tar.gz") => lbfgsbf });
+# get the source, untar it and then delete
+function getSource()
+    run(`wget http://users.iems.northwestern.edu/~nocedal/Software/Lbfgsb.3.0.tar.gz`)
+    run(`tar xf $(currentDirPath)/Lbfgsb.3.0.tar.gz`)
+    run(`rm $(currentDirPath)/Lbfgsb.3.0.tar.gz`)
+end
 
-
-provides(SimpleBuild,
-         (@build_steps begin
-             GetSources(lbfgsbf)
-             mklibdir()
-             runmake()
-          end), lbfgsbf)
-
+getSource()
+mklibdir()
+runmake()
 writeDeps()
          
-@BinDeps.install
